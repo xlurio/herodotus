67e7404d6e6e78f778f88aa3b9837a07413cbbad
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-06 13:41:31 -0300

    feat: implementing the parse_for_db decorator

--- herodotus/decorators.py
+++ herodotus/decorators.py
@@ -1,16 +1,21 @@
 """The module reserved for the decorators available by the Herodutus package"""
 
+from typing import Callable
 from sqlalchemy.orm import Session
+from scrapy.http.response import Response
+from _collections_abc import list_iterator  # type: ignore
 
 
-def parse_for_db(spider_class: type, session: Session):
+def parse_for_db(
+    parse_method: Callable[[Response], list_iterator],
+    response: Response,
+    session: Session,
+):
     """Decorator for scrapy spider for automatically storing data to database
 
     Args:
         spider_class (type): the class of the spider
         session (Session): the SQLAlchemy session to save the data
     """
-    spider = spider_class()
-
-    for result in spider.parse():
+    for result in parse_method(response):
         session.add(result)  # type: ignore
